24b766c484f462fa4d562cad5c91b091eafa13ef
Gene Belford <EJBelford@comcast.net>
2022-10-25 15:14:14 -0400

    updates for 10/25

--- index.js
+++ index.js
@@ -9,11 +9,16 @@
 // Section 26: E-Commerce App
 //         27: Design a Custon Database
 //         28: Production-Grade Authentication
-// Lesson: 383
+//         29: Structuring Javascript Projects
+// Lesson: 398
 //
 //--*----1----*----2----*----3----*----4----*----5----*----6----*----7----*----8
 // NOTES: 
 //------------------------------------------------------------------------------
+// 
+// 
+// apt list --upgradable
+// sudo apt upgrade
 // 
 // nodejs.org/api 
 // node --inspect-brk <prjctNm>
@@ -22,6 +27,7 @@
 // npm install express
 // npm install nodemon
 // npm install cookie-session
+// npm install express-validator
 //
 // clear && npm run dev
 // To stop: <CRTL>-C
@@ -33,9 +39,8 @@ const bodyParser    = require('body-parser');
 const chalk         = require('chalk');
 const cookieSession = require('cookie-session');
 const express       = require('express');
-const users = require('./repositories/users');
 
-const usersRepo = require('./repositories/users');
+const authRouter    = require('./routes/admin/auth');
 
 const prjctNm = "eComm"
 const debug   = 1;    // 0: Off   1: On
@@ -44,9 +49,12 @@ const err     = 0;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({
-    keys: ['nfdvaw3e4rfdqvafnv399d']
-}));
+app.use(
+    cookieSession({
+        keys: ['nfdvaw3e4rfdqvafnv399d']
+    })
+);
+app.use(authRouter);
 
 if (debug > 0) {
     console.log(chalk.yellow('DEBUG: ') + 'Hi there from ' + prjctNm + '!');
@@ -58,107 +66,6 @@ if (debug > 0) {
         };
     };
 };
-
-// Router  
-
-app.get('/signup', (req, res) => {
-    // res.send('Hi theres from ' + prjctNm + ' server!');
-    res.send(`
-        <div>
-            Your ID is: ${req.session.userId}
-            <form method="POST">
-                <input name="email" placeholder="email" />
-                <input name="password" placeholder="password" />
-                <input name="passwordConfirmation" placeholder="password confirmation" />
-                <button>Sign Up</button>
-            </form>
-        </div>
-    `);
-});
-
-/* const bodyParser = (req, res, next) => { 
-    console.log('====================');
-    
-    if (req.method === 'POST') {
-        req.on('data', data => {
-            // console.log(chalk.yellow('DEBUG: Data: ') + data);
-            const parsed = data.toString('utf8').split('&');
-            // console.log(chalk.yellow('DEBUG: Parsed: ') + parsed);
-            const formData = {};
-            for (let pair of parsed) {
-                // console.log(chalk.yellow('DEBUG: ') + pair);
-                const [key, value] = pair.split("=");
-                // console.log(chalk.yellow('DEBUG: Pair: ') + [key, value]);
-                formData[key] = value;
-            };
-            console.log(formData);
-            req.body = formData;
-            next();
-        });
-    } else {
-        next();
-    };
-}; */
-
-app.post('/signup', async (req, res) => {
-    // console.log(req.body);
-    const { email, password, passwordConfirmation } = req.body;
-
-    const existingUser = await usersRepo.getOneBy({ email: email});
-    if (existingUser) {
-        return res.send('Email already in use.');
-    };
-
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords most match.');
-    }
-
-    const user = await usersRepo.create( { email: email, password: password } );
-
-    req.session.userId = user.id; 
-
-    res.send('Account created!');
-});
-
-app.get('/signout', (req, res) => {
-    req.session = null;
-    res.send('You are logged out.');
-});
-
-app.get('/signin', (req, res) => {
-    res.send(`
-        <div>
-            <form method="POST">
-                <input name="email" placeholder="email" />
-                <input name="password" placeholder="password" />
-                <button>Sign In</button>
-            </form>
-        </div>
-    `);
-});
-
-app.post('/signin', async (req, res) => {
-    const { email, password } = req.body;
-
-    const user = await usersRepo.getOneBy({ email: email });
-
-    if (!user) { 
-        return res.send('Email not found');
-    };
-
-    const validPassword = await usersRepo.comparePasswords(
-        user.password,
-        password
-    );
-
-    if (!validPassword) {
-        return res.send('Invalid password');
-    };
-
-    req.session.userId = user.id;
-
-    res.send('You are signed in!');
-});
 
 app.listen(3000, ()=> {
     console.log(chalk.yellow('DEBUG: ') + prjctNm + ' is listening!');
---
+++ routes/admin/auth.js
@@ -0,0 +1,118 @@
+//--*----1----*----2----*----3----*----4----*----5----*----6----*----7----*----8
+//                        Classification: UNCLASSIFIED
+//==============================================================================
+//                Copyright, Belford DBA Consulting, LLC, 2022
+//                      Unpublished, All Rights Reserved
+//==============================================================================
+//--*----|----*----|----*----|----*----|----*----|----*----|----*----|----*----/
+//
+//--*----1----*----2----*----3----*----4----*----5----*----6----*----7----*----8
+// NOTES: 
+//------------------------------------------------------------------------------
+// 
+// clear && npm run dev
+// To stop: <CRTL>-C
+//
+// http://localhost:3000/
+//
+//--*----|----*----|----*----|----*----|----*----|----*----|----*----|----*----/
+
+const express        = require('express');
+const { check, 
+        validationResult }      = require('express-validator');
+
+const usersRepo      = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, 
+        requirePassword, 
+        requirePasswordConfirmation,
+        requiredEmailExists,
+        requireValidPasswordForUser } = require('./validators');
+
+const router  = express.Router();
+
+router.get('/signup', (req, res) => {
+    // res.send('Hi theres from ' + prjctNm + ' server!');
+    res.send(signupTemplate({ req: req }));
+});
+
+/* const bodyParser = (req, res, next) => { 
+    console.log('====================');
+    
+    if (req.method === 'POST') {
+        req.on('data', data => {
+            // console.log(chalk.yellow('DEBUG: Data: ') + data);
+            const parsed = data.toString('utf8').split('&');
+            // console.log(chalk.yellow('DEBUG: Parsed: ') + parsed);
+            const formData = {};
+            for (let pair of parsed) {
+                // console.log(chalk.yellow('DEBUG: ') + pair);
+                const [key, value] = pair.split("=");
+                // console.log(chalk.yellow('DEBUG: Pair: ') + [key, value]);
+                formData[key] = value;
+            };
+            console.log(formData);
+            req.body = formData;
+            next();
+        });
+    } else {
+        next();
+    };
+}; */
+
+router.post('/signup', [
+    requireEmail, 
+    requirePassword,
+    requirePasswordConfirmation
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        // console.log(errors);
+        
+        if (!errors.isEmpty()) {
+            return res.send(signupTemplate({ req, errors }));
+        };
+
+        // console.log(req.body);
+        const { email, password, passwordConfirmation } = req.body;
+        const user = await usersRepo.create( { email: email, password: password } );
+
+        req.session.userId = user.id; 
+
+        res.send('Account created!');
+    }
+);
+
+router.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out.');
+});
+
+router.get('/signin', (req, res) => {
+    res.send(signinTemplate({}));
+});
+
+router.post('/signin', [
+        requiredEmailExists,
+        requireValidPasswordForUser
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        // console.log(errors);
+
+        if (!errors.isEmpty()) {
+            return res.send(signinTemplate({ errors: errors }));
+        }
+
+        const { email } = req.body;
+
+        const user = await usersRepo.getOneBy({ email: email });
+
+        // req.session.userId = user.id;
+
+        res.send('You are signed in!');
+    }
+);
+ 
+module.exports = router;
